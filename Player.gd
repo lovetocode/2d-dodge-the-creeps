72066c7d381e4cfaa2589148b76f1a10ed3f9891
lovetocode <dshaggy28@yahoo.com>
2022-05-07 15:51:21 -0700

    fixed movement of player

--- Player.gd
+++ Player.gd
@@ -11,13 +11,13 @@ func _ready():
 
 func _process(delta):
 	var direction = Vector2.ZERO
-	if Input.is_action_just_pressed("move_right"):
+	if Input.is_action_pressed("move_right"):
 		direction.x += 1
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		direction.x -= 1	
-	if Input.is_action_just_pressed("move_down"):
+	if Input.is_action_pressed("move_down"):
 		direction.y += 1
-	if Input.is_action_just_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
 	
 	
